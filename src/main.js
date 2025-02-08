904846ef64d89636a3607e9055c9dc9072b49adf
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-02-08 22:57:20 +0300

    Подключился к базе данных

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './theme.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { initializeApp } from "firebase/app"; 
+import "./theme.css";
 
-createApp(App).use(store).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ-bx8Jn-Ij6VFQ0liLeLvkRsfQNlZDpg",
+  authDomain: "bank-online-24fd4.firebaseapp.com",
+  projectId: "bank-online-24fd4",
+  storageBucket: "bank-online-24fd4.appspot.com", 
+  messagingSenderId: "639720065545",
+  appId: "1:639720065545:web:1d5f475d2e7bcb33acc6d8",
+  measurementId: "G-D3DN04VWKV",
+};
+
+initializeApp(firebaseConfig);
+
+createApp(App).use(store).use(router).mount("#app");
--- src/store/modules/auth.modules.js
+++ src/store/modules/auth.modules.js
@@ -1,34 +1,52 @@
-const TOKEN_KEY = 'jwt-token'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {error} from '../../utils/error'
+
+const TOKEN_KEY = "jwt-token";
 
 export default {
-    namespaced: true, 
+  namespaced: true,
 
   state() {
     return {
       token: localStorage.getItem(TOKEN_KEY),
     };
   },
+
   mutations: {
     setToken(state, token) {
-        state.token = token
-        localStorage.setItem(TOKEN_KEY, token)
+      state.token = token;
+      localStorage.setItem(TOKEN_KEY, token);
     },
     logout(state) {
-        state.token = null
-        localStorage.removeItem(TOKEN_KEY)
-    }
+      state.token = null;
+      localStorage.removeItem(TOKEN_KEY);
+    },
   },
+
   actions: {
-    async login({commit}) {
-        commit(TOKEN_KEY, 'TEST')
-    }
+    async login({ commit }, payload) {
+      try {
+        const auth = getAuth(); 
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          payload.email,
+          payload.password
+        );
+        const token = await userCredential.user.getIdToken(); 
+
+        commit("setToken", token);
+      } catch (e) {
+        console.log(error);
+      }
+    },
   },
+
   getters: {
     token(state) {
-        return state.token
+      return state.token;
     },
     isAuthenticated(_, getters) {
-        return !!getters.token
-    }
-  }
+      return !!getters.token;
+    },
+  },
 };
